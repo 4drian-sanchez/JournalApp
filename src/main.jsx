798a039d7a17021d7e05c16438ffbc4bd727f776
Adrian <adrian.s.duque@gmail.com>
2023-05-12 19:05:23 -0400

    Comenzando el proyecto

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+import './styles.css'
+import React from 'react'
+import { JournalApp } from './JournalApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <JournalApp/>
+  </React.StrictMode>,
+)
