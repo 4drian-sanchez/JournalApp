8e0ceee1dc898296a07068cfad9777cb7c4eb96e
Adrian <adrian.s.duque@gmail.com>
2023-05-13 14:32:04 -0400

    agregando cambios en la rama material UI

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,8 @@
+export const JournalPage = () => {
+  return (
+    <div>
+      <h1>Desde Material UI</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias, consequatur.</p>
+    </div>
+  )
+}
